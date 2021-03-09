507842d09732e0e9f4eca4e5851fa23815a3e083
Weisiang <tm455340@gmail.com>
2021-03-09 11:09:47 +0800

    vue config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/home/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/home/' : '/',
+  assetsDir: process.env.NODE_ENV === 'production' ? 'static' : ''
 }
